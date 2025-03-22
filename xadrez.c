253c0429411e5da22fe6b4ab7aa921c57b4d0fd4
Guilherme Assi <guiassi@hotmail.com>
2025-03-22 11:08:22 -0300

    feat: logica xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,91 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+//Recebe a quantidade de casas para mover e um char identintificador de peça. Realizando a chamada de funções (Recursividade) até a quantidade de casas para mover ser <= 0.
+void moverPeca(int casasParaMover, char peca){
+    if(casasParaMover <= 0)
+        return;
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    switch (peca)
+    {
+    case 't':
+        printf("Direita.\n");
+        moverPeca(casasParaMover-1, peca);
+        break;
+    case 'b':
+        printf("Cima, Direita. \n");
+        moverPeca(casasParaMover-1, peca);
+        break;
+    case 'r':
+        printf("Esquerda.\n");
+        moverPeca(casasParaMover-1, peca);
+        break;
+    default:
+    printf("Opção inválida!\n");
+        break;
+    }
+}
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+int main()
+{
+    const int movimentacaoTorre = 5;
+    const int movimentacaoBispo = 5;
+    const int movimentacaoRainha = 8;
+    const int movimentacaoCavaloHorizontalOuVertical = 2;
+    // Informe "e" ou "d" para movimentar para esquerda ou direita, respectivamente.
+    const char movimentoCavaloPerpendicular = 'd';
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("------------------------------------------------------------- \n");
+    printf("----------------------- Xadrez ------------------------------ \n");
+    printf("------------------------------------------------------------- \n \n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("Movimentação Torre:\n \n");
+    moverPeca(movimentacaoTorre, 't');
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    printf("\n \nMovimentação Bispo:\n \n");
+    moverPeca(movimentacaoBispo, 'b');
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    printf("\n \nMovimentação Rainha:\n \n");
+    moverPeca(movimentacaoRainha, 'r');
+
+    printf("\n \nCavalo irá se movimentar um total de %d casas na vertical ou horizontal e 1 casa perpendicularmente. \n", movimentacaoCavaloHorizontalOuVertical);
+    printf("Movimentação Cavalo:\n \n");
+
+    //Movimentação padrão do cavalo.
+    for (int cavalo = 0; cavalo < movimentacaoCavaloHorizontalOuVertical; cavalo++)
+    {
+        printf("Cavalo:");
+        printf("Cima.\n");
+
+        // Valida se já está na hora de mover perpendicular
+        if (cavalo == (movimentacaoCavaloHorizontalOuVertical - 1))
+        {
+            int moverPerpendicular = 0;
+            while (!moverPerpendicular)
+            {
+                printf("Cavalo:");
+                switch (movimentoCavaloPerpendicular)
+                {
+                case 'e':
+                    printf("Esquerda\n");
+                    break;
+                case 'd':
+                    printf("Direita\n");
+                    break;
+                default:
+                    printf("Opção inválida!\n");
+                    break;
+                }
+
+                // Define como movido.
+                moverPerpendicular = 1;
+            }
+        }
+    };
+
+    printf("\n---------------------------------------------------------------------------------- \n");
+    printf("----------------------- Programa Encerrado! -------------------------------------- \n");
+    printf("---------------------------------------------------------------------------------- \n \n");
 
     return 0;
 }
